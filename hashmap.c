d7faf5524affe4e485428fe79549135baf3ab385
GeraldEspinoza <>
2022-04-09 19:32:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -85,7 +85,8 @@ HashMap * createMap(long capacity)
   HashMap *map = (HashMap * ) malloc (sizeof(HashMap));
   map->buckets = (Pair **) malloc (capacity * sizeof(Pair*));
     map->size = 0;
-    
+    map->capacity = capacity;
+    map->current = -1;
 
     return map;
 
