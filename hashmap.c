df9a70687a944ecffec67968c4d3296e2ea87f78
GeraldEspinoza <>
2022-04-09 16:59:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -113,7 +113,7 @@ Pair * firstMap(HashMap * map) {
           if(map->buckets[i]->key != NULL)
           {
             map->current = i;
-            return map->bucket[map->current];
+            return map->bucket[i];
           }
          }
 
